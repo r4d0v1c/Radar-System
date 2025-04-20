088d71a8068650d4b61e72e2fd422625b114ecfe
r4d0v1c <lukarad9@gmail.com>
2025-04-20 15:22:07 +0200

    imported Servo library at made it sweep from left to right and reverse

--- radarControl/src/main.cpp
+++ radarControl/src/main.cpp
@@ -1,9 +1,22 @@
 #include <Arduino.h>
+#include "Servo.h"
+
+Servo myServo;
 
 void setup() {
   Serial.begin(9600);
+  myServo.attach(3); // Attach the servo on pin 3
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  for (int pos = 0; pos <= 180; pos += 1) { // Sweep from 0 to 180 degrees
+    myServo.write(pos); // Set the servo position
+    delay(15); // Wait for the servo to reach the position
+    Serial.print(pos);
+  }
+  for (int pos = 180; pos >= 0; pos -= 1) { // Sweep from 180 to 0 degrees
+    myServo.write(pos); // Set the servo position
+    delay(15); // Wait for the servo to reach the position
+    Serial.print(pos);
+  }
 }
